ffc45e12d6bedaf58ba102b29424021851c61ad4
tariramadhanty <167733100+tariramadhanty@users.noreply.github.com>
2024-05-27 07:21:10 +0700

    Update 026_Elva_rekursif.cpp

--- 026_Elva_rekursif.cpp
+++ 026_Elva_rekursif.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 
-//fungsi menghitung fpb dengan Algoritma Euclidean
-int hitungfpb(int a, int b) { //fungsi hitungfpb dengan parameter a dan b
-    if (b==0)//jika b=0
-            return a;//maka nilai a adalah FPB dari a dan b
-                else
-                        return hitungfpb(b,a%b);//jika b!=0, maka fungsi hitungfpb dipanggil kembali dengan parameter b dan a mod b
-              } 
-
-int main() {
-    int m,n;
-        cout<<"m: ";//input m
-        cin>>m;//memasukkan input ke variabel m     
-        cout<<"n: ";//input n
-        cin>>n;//memasukkan input ke variabel n
-        int fpb = hitungfpb(m,n);//menghitung fpb dengan memanggil fungsi hitungfpb dengan parameter m dan n
-        cout << "FPB: " << fpb << endl;//output fpb
+int EuclideanGCD(int m, int n) 
+{
+    if (n == 0) 
+    {
+        return m;
+    } 
+    else 
+    {
+        return EuclideanGCD(n, m % n);
     }
+}
+
+int main() 
+{
+    int bil1, bil2;
+    
+    cout << "Masukkan bilangan pertama: ";
+    cin >> bil1;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> bil2;
+
+    cout << "FPB dari " << bil1 << " dan " << bil2 << " adalah: " << EuclideanGCD(bil1, bil2) << endl;
+    
+    return 0;
+}
